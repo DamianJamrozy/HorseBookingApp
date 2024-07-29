4fdd07df943902d2cfdad69043bd8ebc8e62e824
Patryk Arendt <patryk.arendt99@gmail.com>
2024-07-29 11:20:25 +0200

    Add rezerwation fix

--- scripts/get_available_trainers.php
+++ scripts/get_available_trainers.php
@@ -9,7 +9,7 @@ if ($conn->connect_error) {
 $start_time = $_POST['start_time'];
 $end_time = $_POST['end_time'];
 
-$sql = "SELECT id, imie, nazwisko FROM users WHERE rola = 'trener' AND id NOT IN (
+$sql = "SELECT id, imie, nazwisko FROM users WHERE rola = '2' AND id NOT IN (
     SELECT trener_id FROM reservations WHERE ('$start_time' BETWEEN data_rezerwacji_od AND data_rezerwacji_do)
     OR ('$end_time' BETWEEN data_rezerwacji_od AND data_rezerwacji_do)
     OR (data_rezerwacji_od BETWEEN '$start_time' AND '$end_time')
