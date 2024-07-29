606482e2231e00d4feb114e0e3411954a3a2e324
Damian Jamroży <damianjamrozy99@gmail.com>
2024-07-29 02:07:45 +0200

    Porządki

--- V 2.0/scripts/logout.php
+++
@@ -1,7 +0,0 @@
-<!-- plik który zarządza wylogowaniem poprzez klikniecie logout   -->
-<?php
-session_start();
-session_unset();
-session_destroy();
-header("Location: ../sites/index.php");
-exit();
